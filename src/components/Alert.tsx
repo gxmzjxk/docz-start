ddedb1b19c2ba9cad90a168ec15076c0f80de87b
jiangxiaokun <jiangxiaokun@bytedace.com>
2019-10-27 00:32:09 +0800

    feat: support less

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,32 +1,24 @@
-import React, { SFC } from 'react'
-import styled from '@emotion/styled'
+import React, { FC } from 'react';
 
-export type Kind = 'info' | 'positive' | 'negative' | 'warning'
-export type KindMap = Record<Kind, string>
-
-const kinds: KindMap = {
-  info: '#5352ED',
-  positive: '#2ED573',
-  negative: '#FF4757',
-  warning: '#FFA502',
-}
+import './Alert.less';
 
 export interface AlertProps {
-  /**
+    /**
    * Set this to change alert kind
    * @default info
    */
-  kind: 'info' | 'positive' | 'negative' | 'warning'
+    kind: 'info' | 'positive' | 'negative' | 'warning';
 }
 
-const AlertStyled = styled('div')<AlertProps>`
-  padding: 15px 20px;
-  background: white;
-  border-radius: 3px;
-  color: white;
-  background: ${({ kind = 'info' }) => kinds[kind]};
-`
+export const Alert: FC<AlertProps> = ({ children, kind, ...rest }) => {
+    return (
+        <div className="Alert" {...rest}>
+            {children}
+        </div>
+    );
+};
+Alert.defaultProps = {
+    kind: 'info'
+};
 
-export const Alert: SFC<AlertProps> = ({ kind, ...props }) => (
-  <AlertStyled {...props} kind={kind} />
-)
+export default Alert;
